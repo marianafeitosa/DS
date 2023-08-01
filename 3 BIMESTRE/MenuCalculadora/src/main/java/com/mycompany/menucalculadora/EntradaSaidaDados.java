28010ca4ebad147a536cc8d811baeef1141c90c1
mar¡ <99839074+marianafeitosa@users.noreply.github.com>
2023-07-31 23:59:14 -0300

    Add files via upload

---
+++ 3 BIMESTRE/MenuCalculadora/src/main/java/com/mycompany/menucalculadora/EntradaSaidaDados.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.menucalculadora;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author marianaarashiro
+ */
+public class EntradaSaidaDados {
+     //metodo que realiza a entrada de dados
+    public String entradaDados(String mensagemEntrada){
+        return JOptionPane.showInputDialog(mensagemEntrada);
+    }
+    
+    //metodo que realiza a saida de dados
+    public void saidaDados(String mensagemSaida){
+        JOptionPane.showMessageDialog(null, mensagemSaida);
+    }
+    
+}
